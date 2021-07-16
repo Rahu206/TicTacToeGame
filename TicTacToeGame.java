0c85e64aae17a3e51de90b4c3ac90f86d01122b9
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-16 19:36:07 +0530

    UC2-choose a  letter X or O feature added

---
+++ TicTacToeGame.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.tictactoe;
+
+import java.util.Scanner;
+
+public class TicTacToeGame {
+	
+	public static char playerSymbol;
+	public static char cpuSymbol;
+	
+	public static char[] createBoard() {
+		char[] board = new char[10];
+		for(int i=0;i<9;i++) {
+			board[i]= '-';
+		}
+		return board;	
+	}
+	
+	public static void main(String[] args) {
+		char[] board = createBoard();
+		System.out.println("Select the Letter you wanted \n  press 1 . Choose X \n  Press 2 . Choose O\n\nEnter your Choice : ");
+		Scanner scan =new Scanner(System.in);
+		int choice = scan.nextInt();
+		switch(choice){
+			case 1 : {
+				playerSymbol='X';
+				cpuSymbol='O';
+				break;
+			}
+			case 2 :{
+				playerSymbol='O';
+				cpuSymbol='X';
+				break;
+			}
+			default: {
+				System.out.println("Invalid Choice . Retry Again!");
+				return;
+			}
+			
+		}
+	
+	}
+
+}
+
+
